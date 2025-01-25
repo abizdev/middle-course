875d581689bee03a9f1a686c91f9149c57bdf3c2
Abdulaziz <abizlancer@gmail.com>
2025-01-25 18:44:32 +0500

    feat: base app link

---
+++ src/shared/ui/app-link/app-link.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link, LinkProps } from 'react-router';
+import { classNames } from 'shared/lib';
+import * as styles from './app-link.module.scss';
+
+export enum AppLinkTheme {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+}
+
+interface Props extends LinkProps {
+  theme: AppLinkTheme;
+  className?: string;
+}
+
+const AppLink: React.FC<Props> = (props) => {
+  const {
+    theme = AppLinkTheme.PRIMARY,
+    className = '',
+    to,
+    children,
+    ...otherProps
+  } = props;
+
+  return (
+    <Link
+      to={to}
+      className={classNames(styles.AppLink, {}, [className, styles[theme]])}
+      {...otherProps}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default AppLink;
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,12 @@
 import path from 'path';
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 import { BuildEnv, BuildMode, BuildPaths } from './config/types/config';
 import buildWebpackConfig from './config/build-webpack-config';
 import 'webpack-dev-server';
 
 export default (env: BuildEnv) => {
-  const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-  const __dirname = path.dirname(__filename);
+  // const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+  // const __dirname = path.dirname(__filename);
 
   const mode: BuildMode = env.mode || 'development';
   const port: number = env.port || 4000;
