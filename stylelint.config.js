423a02bd34d09a0feac7f3614179ed90a5f16893
Abdulaziz <abizlancer@gmail.com>
2025-01-26 00:05:40 +0500

    feat: style linter and changed theme colors

---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+export default {
+	extends: ["stylelint-config-standard-scss"],
+	rules: {
+		'selector-class-pattern': null,
+		'block-no-empty': true
+	}
+};
